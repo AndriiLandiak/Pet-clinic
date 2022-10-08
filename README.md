966021f608562e347d89f0aa39fef4e9241246b2
AndriiLandiak <andriylandiak@gmail.com>
2022-10-08 19:26:57 +0300

    Doing some SHIT

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Pet-clinic
+This is a s1mple-sample project of Spring Framework!
---
+++ data/src/main/java/com/example/petclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package com.example.petclinic.model;
+
+public class PetType {
+    private String pateName;
+
+    public String getPateName() {
+        return pateName;
+    }
+
+    public void setPateName(String pateName) {
+        this.pateName = pateName;
+    }
+}
